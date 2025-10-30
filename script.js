d21f779ff8d21061cf64e23c03f27021accb442c
SaMeSparts <132626666+SaMeSparts@users.noreply.github.com>
2025-10-30 13:32:12 -0600

    se acabo el programa utilizando js css

---
+++ script.js
@@ -0,0 +1,8 @@
+function cambiar_imagen(imagen) {
+    document.getElementById('luigi').src = imagen;
+}
+
+function cambiar_fondo(color) {
+    document.body.style.backgroundColor = color;
+    
+}
